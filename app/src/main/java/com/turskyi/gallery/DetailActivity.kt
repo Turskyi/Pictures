04e72ba41b409b09c8d142a4a4a0a9f690e23bcf
Dmytro Turskyi <dmitriy.turskiy@gmail.com>
2019-07-16 17:03:09 +0300

    Folders without action

--- app/src/main/java/com/turskyi/gallery/DetailActivity.kt
+++ app/src/main/java/com/turskyi/gallery/DetailActivity.kt
@@ -1,21 +1,107 @@
 package com.turskyi.gallery
 
 import android.os.Bundle
+import android.os.Environment
 import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.activity_main.*
+import java.io.File
+
 
 class DetailActivity : AppCompatActivity() {
 
-    private lateinit var aFolder: ImageView
+    private lateinit var path:String
+
+    lateinit var name: String
+
+    private lateinit var aFile: ImageView
+
+
+    lateinit var aRecyclerView: RecyclerView
+
+    private var quantityOfColumns: Int = 1
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_detail)
+        setContentView(R.layout.activity_main)
 
-        aFolder = findViewById(R.id.image_view)
-        val aBundle:Bundle? = intent.extras
-        if (aBundle != null) {
-            aFolder.setImageResource(aBundle.getInt("Image"))
+//        aFile = findViewById(R.id.image_view_preview)
+//        val aBundle:Bundle? = intent.extras
+//        if (aBundle != null) {
+//            aFile.setImageResource(aBundle.getInt("File"))
+//        }
+
+//        name = findViewById(R.id.file_name)
+//        val aBundle:Bundle? = intent.extras
+//        if (aBundle != null) {
+//            name
+//        }
+
+        path = "/storage/emulated/0/${getFileName()}"
+
+        val path = intent.getStringExtra("path")
+        if (path != null) {
+            this.path = path
         }
+
+        increment_columns.setOnClickListener {
+            incrementOfColumns()
+        }
+
+        decrement_columns.setOnClickListener {
+            decrementOfColumns()
+        }
+
+
+        aRecyclerView = findViewById(R.id.recycler_view)
+
+        getNumberOfColumns()
+
+    }
+
+    private fun getNumberOfColumns() {
+        val aGridLayoutManager = GridLayoutManager(this, quantityOfColumns)
+        aRecyclerView.layoutManager = aGridLayoutManager
+    }
+
+    /**
+     * This method is called when the plus button is clicked.
+     */
+    private fun incrementOfColumns() {
+        if (quantityOfColumns == 10) {
+            return
+        }
+        quantityOfColumns += 1
+        displayQuantityOfColumns(quantityOfColumns)
+    }
+
+    /**
+     * This method is called when the minus button is clicked.
+     */
+    private fun decrementOfColumns() {
+        if (quantityOfColumns == 1) {
+            return
+        }
+        quantityOfColumns -= 1
+        displayQuantityOfColumns(quantityOfColumns)
+    }
+
+    /**
+     * This method displays the given quantity value on the screen.
+     */
+    private fun displayQuantityOfColumns(quantityOfColumns: Int) {
+        quantity_text_view.text = "$quantityOfColumns"
+        getNumberOfColumns()
+    }
+
+    private fun getFileName(): String {
+        val filepath = Environment.getExternalStorageDirectory().getPath()
+        val file = File(filepath )
+        if (!file.exists()) {
+            file.mkdirs()
+        }
+        return file.getAbsolutePath() + "/"
     }
 }
--- app/src/main/java/com/turskyi/gallery/adapter/RecyclerAdapter.kt
+++ app/src/main/java/com/turskyi/gallery/adapter/RecyclerAdapter.kt
@@ -9,8 +9,8 @@ import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
-import com.turskyi.gallery.R
 import com.turskyi.gallery.DetailActivity
+import com.turskyi.gallery.R
 import java.io.File
 
 
@@ -21,7 +21,7 @@ class RecyclerAdapter(
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): FileViewHolder {
         val view = LayoutInflater.from(parent.context).inflate(
-            R.layout.list_item,
+            R.layout.file_list,
             parent, false
         )
         return FileViewHolder(view)
@@ -32,7 +32,7 @@ class RecyclerAdapter(
     }
 
     override fun onBindViewHolder(holder: FileViewHolder, position: Int) {
-holder.bindView(aFileList[position])
+        holder.bindView(aFileList[position],aContext)
 
 //        holder.aFolder.setImageResource(aFileList[position])
 //        holder.aFolder.setOnClickListener {
@@ -44,15 +44,22 @@ holder.bindView(aFileList[position])
     }
 
 
+    class FileViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        private val fileNameTV: TextView = itemView.findViewById(R.id.file_name)
+        var aFile: ImageView = itemView.findViewById(R.id.image_view_preview)
+        fun bindView(file: File,aContext: Context) {
+            fileNameTV.text = file.name
 
-        class FileViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-            private val fileNameTV: TextView = itemView.findViewById(R.id.file_name)
-//            var aFolder: ImageView = itemView.findViewById(R.id.image_view_preview)
-            fun bindView(file:File) {
-    fileNameTV.text = file.name
-}
+            aFile.setOnClickListener {
+                val anIntent = Intent(aContext, DetailActivity::class.java)
+                anIntent.putExtra("File", file.path)
+                aContext.startActivity(anIntent)
+            }
+
+
         }
     }
+}
 
 
 
